50db75bcb38a35e0765cc375f07804bc02423d67
JDjuxx <juan.guevara01@epn.edu.ec>
2020-01-14 12:19:00 -0500

    Libros:Titulo,Link,Precio

--- Scrapy/03-arania-basica/basicSpider/basicSpider/spiders/spider.py
+++ Scrapy/03-arania-basica/basicSpider/basicSpider/spiders/spider.py
@@ -6,6 +6,11 @@ class IntroSpider(scrapy.Spider):
         'http://books.toscrape.com/catalogue/category/books/travel_2/index.html'
     ]
 
+    books_info = {
+        'title':[],
+        'link':[],
+        'price':[]
+    }
     def start_requests(self):
         for url in self.urls:
             yield scrapy.Request(url=url)
@@ -13,11 +18,39 @@ class IntroSpider(scrapy.Spider):
     def parse(self, response):
         tag_container = response.css('article.product_pod')
         titles = tag_container.css('h3 > a::text').extract()
+        self.fill_titles(titles)
         url_books = tag_container.css('h3 > a::attr(href)').extract()
-        for url in url_books:
-            self.urls.append(response.urljoin(url))
-        ##url_books_complete = response.urljoin(url_books)
-        print(self.urls)
+        self.fill_links(url_books,response)
+        prices = tag_container.css('p.price_color::text').extract()
+        self.fill_prices(prices)
+        self.create_file()
+        print(self.books_info)
+    
+    def fill_titles(self, titles):
+        for title in titles:
+            self.books_info['title'].append(title)
+    
+    def fill_links(self, links, response):
+        for url in links:
+            self.books_info['link'].append(response.urljoin(url))
+    
+    def fill_prices(self, prices):
+        for price in prices:
+            character, value = price.split("£",1)
+            self.books_info['price'].append(value)
+
+    def create_file(self):
+        f = open("books.txt","w+")
+        titles = self.books_info['title']
+        links = self.books_info['link']
+        prices = self.books_info['price']
+        for i in range(titles.__len__()):
+            f.write('Book Title: '+titles[i]+'\tLink: '+links[i]+'\tPrice: '+prices[i]+'\t\n')
+        f.close()
+
+
+
+
         
 
 
