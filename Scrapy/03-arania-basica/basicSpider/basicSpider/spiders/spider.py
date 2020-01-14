11353ae16184f77c8b6b8472ac0da19eb840dafc
JDjuxx <juan.guevara01@epn.edu.ec>
2020-01-14 15:22:07 -0500

    Deber Scrapy

--- Scrapy/03-arania-basica/basicSpider/basicSpider/spiders/spider.py
+++ Scrapy/03-arania-basica/basicSpider/basicSpider/spiders/spider.py
@@ -2,29 +2,36 @@ import scrapy
 
 class IntroSpider(scrapy.Spider):
     name = 'introduccion_spider'
-    urls = [
-        'http://books.toscrape.com/catalogue/category/books/travel_2/index.html'
-    ]
-
+    principal_url = 'http://books.toscrape.com/catalogue/category/books_1/index.html'
+    urls = []
     books_info = {
         'title':[],
         'link':[],
         'price':[]
     }
+    flag = True
     def start_requests(self):
-        for url in self.urls:
-            yield scrapy.Request(url=url)
+        yield scrapy.Request(self.principal_url)
     
     def parse(self, response):
-        tag_container = response.css('article.product_pod')
-        titles = tag_container.css('h3 > a::text').extract()
-        self.fill_titles(titles)
-        url_books = tag_container.css('h3 > a::attr(href)').extract()
-        self.fill_links(url_books,response)
-        prices = tag_container.css('p.price_color::text').extract()
-        self.fill_prices(prices)
-        self.create_file()
-        print(self.books_info)
+        if(self.flag):
+            tag_container = response.css('aside.sidebar.col-sm-4.col-md-3')
+            url_gender = tag_container.css('ul.nav.nav-list > li > ul > li > a::attr(href)').extract()
+            for url in url_gender:
+                self.urls.append(response.urljoin(url))
+            self.flag = False
+            for url in self.urls:
+                yield scrapy.Request(url=url)
+        else:
+            tag_container = response.css('article.product_pod')
+            titles = tag_container.css('h3 > a::text').extract()
+            self.fill_titles(titles)
+            url_books = tag_container.css('h3 > a::attr(href)').extract()
+            self.fill_links(url_books,response)
+            prices = tag_container.css('p.price_color::text').extract()
+            self.fill_prices(prices)
+            self.create_file()
+            
     
     def fill_titles(self, titles):
         for title in titles:
@@ -47,12 +54,3 @@ class IntroSpider(scrapy.Spider):
         for i in range(titles.__len__()):
             f.write('Book Title: '+titles[i]+'\tLink: '+links[i]+'\tPrice: '+prices[i]+'\t\n')
         f.close()
-
-
-
-
-        
-
-
-
-
