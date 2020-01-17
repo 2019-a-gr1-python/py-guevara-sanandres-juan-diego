e8218073cb989f248f9143f4cf2bb300c4bf9c59
JDjuxx <juan.guevara01@epn.edu.ec>
2020-01-17 18:18:06 -0500

    Solucion precios

--- Scrapy/03-arania-basica/basicSpider/basicSpider/spiders/spider.py
+++ Scrapy/03-arania-basica/basicSpider/basicSpider/spiders/spider.py
@@ -10,14 +10,16 @@ class IntroSpider(scrapy.Spider):
         tag_container = response.css('article.product_pod')
         titles = tag_container.css('h3 > a::text').extract()
         url_books = tag_container.css('a > img::attr(src)').extract()
-        prices = tag_container.css('p.price_color::text').extract()
-        self.create_file(titles,url_books,prices)
+        prices = tag_container.css('p.price_color::text').extract()  
+        self.create_file(titles,url_books,prices,response)
         dep_url = response.css('li.next > a::attr(href)').extract()
         if(dep_url):
             yield scrapy.Request(response.urljoin(dep_url[0]))
              
-    def create_file(self,titles,links,prices):
-        f = open("books.txt","a")
+    def create_file(self,titles,links,prices,response):
+        f = open("books.txt","a+")
         for i in range(len(titles)):
-            f.write('Book Title: '+titles[i]+'\tLink: '+links[i]+'\tPrice: '+prices[i]+'\t\n')
+            character, value = prices[i].split("£",1)
+            links[i] = response.urljoin(links[i])
+            f.write("Book Title: "+titles[i]+"\tLink: "+links[i]+"\tPrice: "+value+"\t\n")
         f.close()
